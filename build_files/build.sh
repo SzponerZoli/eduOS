3aebcc878200a8a78da7ac5cbe58befcf1a27d42
--help <szponerzolidev@proton.me>
2026-06-03 22:22:28 +0200

    auto-disabling veyon and removing veyon desktop entries so students cant access it easily

--- build_files/build.sh
+++ build_files/build.sh
@@ -30,5 +30,8 @@ dnf5 install -y chromium
 dnf5 install -y kwrite
 dnf5 install -y vlc
 dnf5 install -y https://github.com/veyon/veyon/releases/download/v4.10.3/veyon-4.10.3.0-fedora.44.x86_64.rpm # Installs Veyon (temp until not available in fedora repos or copr)
+# Disabling Veyon
+systemctl disable veyon.service
+rm /usr/share/applications/veyon-master.desktop
+rm /usr/share/applications/veyon-configurator.desktop
 
-systemctl enable podman.socket
